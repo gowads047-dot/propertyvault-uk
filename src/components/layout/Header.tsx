099e7d8122eb0837bf6c81e8f40a6bb495b9c1f1
Nass <gowads047@gmail.com>
2026-06-24 18:45:59 +0100

    fix: update Academy nav label from 'Free Sign Up' to '£14.99/mo'

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const mainLinks = [
   { href: "/calculators", label: "Calculators", sub: "" },
   { href: "/templates", label: "Templates", sub: "" },
   { href: "/blog", label: "Blog", sub: "" },
-  { href: "/academy", label: "Academy", sub: "Free Sign Up" },
+  { href: "/academy", label: "Academy", sub: "£14.99/mo" },
   { href: "/rentura", label: "Rentura", sub: "Early Access", highlight: true },
 ];
 
